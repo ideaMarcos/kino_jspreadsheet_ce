3bbbba6763c036b9298f6ad571bf44554d28fcb9
ideaMarcos <571086+ideaMarcos@users.noreply.github.com>
2026-03-13 22:15:27 -0400

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule KinoJspreadsheetCe.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Jspreadsheet CE integration for Livebook"
 
   def project do
